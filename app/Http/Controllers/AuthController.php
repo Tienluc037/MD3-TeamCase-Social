94b5b3329936c0e9dc34c21903a0c164a6aea5ec
trieu.long <long.trieu.hr@gmail.com>
2022-03-16 10:26:02 +0700

    update form login, LoginController return view welcome

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\AuthService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class AuthController extends Controller
+{
+    public $authService;
+
+    public function __construct(AuthService $authService)
+    {
+        return $this->authService = $authService;
+    }
+
+    public function showFormLogin()
+    {
+        return view('backend.auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        if($this->authService->login($request)){
+            return view('welcome');
+        }
+        else{
+            Session::flash('msg','Tài khoản hoặc mật khẩu sai');
+            return redirect()->back();
+        }
+    }
+
+    public function logout()
+    {
+        $this->authService->logout();
+
+    }
+}
