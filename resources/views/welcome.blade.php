699bb6a4a45e6fe36441367cc68ce5d3543ac36f
Tienluc037 <nguyentienluc12a3@gmail.com>
2022-03-18 11:47:54 +0700

    update img

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -91,6 +91,7 @@
 {{--                                       onchange="document.getElementById('img').src = window.URL.createObjectURL(this.files[0])">--}}
 {{--                                        <input type="file" name="image" class="form-control">--}}
 {{--                            </div>--}}
+
                                     <button type="submit" class="btn btn-default">Submit</button>
 
 
